1bbbc904e7f9e8cbb200e2dcda8ea5d75ef2c960
Luis Minaya <luis.minaya@gmail.com>
2025-04-12 20:42:51 -0400

    Completed part 1

--- model.py
+++ model.py
@@ -1,11 +1,72 @@
 """Models for movie ratings app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 # Replace this with your code!
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, 
+                        autoincrement = True,
+                        primary_key = True)
+    email = db.Column(db.String,
+                      unique = True)
+    password = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="user")
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+    
+
+class Movie(db.Model):
+    """ A movie"""
+
+    __tablename__ = 'movies'
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement = True,
+                        primary_key = True)
+    
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="movie")
+
+    def __repr__(self):
+        return f"""<Movie title={self.title} overview={self.overview} 
+            release_date={self.release_date} poster_path={self.poster_path}"""
+    
+
+class Rating(db.Model):
+    """ Move ratings """
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, 
+                          autoincrement = True,
+                          primary_key = True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, 
+                        db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+    
+    movie = db.relationship("Movie", back_populates='ratings')
+    user = db.relationship("User", back_populates='ratings')
+
+    def __repr__(self):
+        return f"<Rating id={self.rating_id} score={self.score}>"
+
+    
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
